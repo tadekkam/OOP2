10d4ce4a6cbc1b58c01dfdffa205a4618f17b16a
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-25 00:01:16 +0100

    ADD: Task1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WindowMessage windowMessage = new WindowMessage();
+        ConsoleMessage consoleMessage = new ConsoleMessage();
+
+        System.out.println(windowMessage.getMessage());
+        System.out.println(consoleMessage.getMessage());
+
+    }
+}
+
+
---
+++ src/main/java/org/example/WindowMessage.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class WindowMessage extends Message {
+    @Override
+    String getMessage() {
+        return "This is class WindowMessage";
+    }
+}
